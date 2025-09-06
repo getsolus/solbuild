getsolus/solbuild

commit ae36f4872a6804b89f1ab4f74d04d67a36b459cd

Reformat .golangci.yaml to work with their new configuration format (#130)
Co-authored-by: Silke Hofstra <silke@slxh.eu>

builder/manager.go
@@ -37,25 +37,25 @@ import (
 var (
 	// ErrManagerInitialised is returned when the library user attempts to set
 	// a core part of the Manager after it's already been initialised.
-	ErrManagerInitialised = errors.New("The manager has already been initialised")
+	ErrManagerInitialised = errors.New("the manager has already been initialised")
 
 	// ErrNoPackage is returned when we've got no package.
-	ErrNoPackage = errors.New("You must first set a package to build it")
+	ErrNoPackage = errors.New("you must first set a package to build it")
 
 	// ErrNotImplemented is returned as a placeholder when developing functionality.
-	ErrNotImplemented = errors.New("Function not yet implemented")
+	ErrNotImplemented = errors.New("function not yet implemented")
 
 	// ErrProfileNotInstalled is returned when a profile is not yet installed.
-	ErrProfileNotInstalled = errors.New("Profile is not installed")
+	ErrProfileNotInstalled = errors.New("profile is not installed")
 
 	// ErrInvalidProfile is returned when there is an invalid profile.
-	ErrInvalidProfile = errors.New("Invalid profile")
+	ErrInvalidProfile = errors.New("invalid profile")
 
 	// ErrInvalidImage is returned when the backing image is unknown.
-	ErrInvalidImage = errors.New("Invalid image")
+	ErrInvalidImage = errors.New("invalid image")
 
 	// ErrInterrupted is returned when the build is interrupted.
-	ErrInterrupted = errors.New("The operation was cancelled by the user")
+	ErrInterrupted = errors.New("the operation was cancelled by the user")
 )
 
 // A Manager is responsible for cleanly managing the entire session within solbuild,
@@ -118,6 +118,7 @@ func NewManager() (*Manager, error) {
 func (m *Manager) SetActivePID(pid int) {
 	m.lock.Lock()
 	defer m.lock.Unlock()
+
 	m.activePID = pid
 }
 
@@ -126,6 +127,7 @@ func (m *Manager) SetActivePID(pid int) {
 func (m *Manager) SetManifestTarget(target string) {
 	m.lock.Lock()
 	defer m.lock.Unlock()
+
 	m.manifestTarget = strings.TrimSpace(target)
 }
 
@@ -251,6 +253,7 @@ func (m *Manager) IsCancelled() bool {
 func (m *Manager) SetCancelled() {
 	m.lock.Lock()
 	defer m.lock.Unlock()
+
 	m.cancelled = true
 }
 
@@ -264,8 +267,10 @@ func (m *Manager) Cleanup() {
 	}
 
 	slog.Debug("Acquiring global lock")
+
 	m.lock.Lock()
 	defer m.lock.Unlock()
+
 	slog.Debug("Cleaning up")
 
 	if m.pkgManager != nil {
@@ -371,16 +376,13 @@ func (m *Manager) Build() error {
 		return ErrInterrupted
 	}
 
-	m.lock.Lock()
-	if m.pkg == nil {
-		m.lock.Unlock()
-		return ErrNoPackage
+	if err := m.checkPackage(); err != nil {
+		return err
 	}
-	m.lock.Unlock()
 
 	// Now get on with the real work!
-	defer m.Cleanup()
 	m.SigIntCleanup()
+	defer m.Cleanup()
 
 	// Now set our options according to the config
 	m.overlay.EnableTmpfs = m.Config.EnableTmpfs
@@ -404,14 +406,17 @@ func (m *Manager) Chroot() error {
 	}
 
 	m.lock.Lock()
+
 	if m.pkg == nil {
 		m.lock.Unlock()
 		return ErrNoPackage
 	}
+
 	m.lock.Unlock()
 
 	// Now get on with the real work!
 	defer m.Cleanup()
+
 	m.SigIntCleanup()
 
 	if err := m.doLock(m.overlay.LockPath, "chroot"); err != nil {
@@ -427,23 +432,12 @@ func (m *Manager) Update() error {
 		return ErrInterrupted
 	}
 
-	m.lock.Lock()
-	if m.image == nil {
-		m.lock.Unlock()
-		return ErrInvalidProfile
+	if err := m.prepareUpdate(); err != nil {
+		return err
 	}
 
-	if !m.image.IsInstalled() {
-		m.lock.Unlock()
-		return ErrProfileNotInstalled
-	}
-
-	m.updateMode = true
-	m.pkgManager = NewEopkgManager(m, m.image.RootDir)
-	m.lock.Unlock()
-
-	defer m.Cleanup()
 	m.SigIntCleanup()
+	defer m.Cleanup()
 
 	if err := m.doLock(m.image.LockPath, "updating"); err != nil {
 		return err
@@ -452,22 +446,37 @@ func (m *Manager) Update() error {
 	return m.image.Update(m, m.pkgManager)
 }
 
+func (m *Manager) prepareUpdate() error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if m.image == nil {
+		return ErrInvalidProfile
+	}
+
+	if !m.image.IsInstalled() {
+		return ErrProfileNotInstalled
+	}
+
+	m.updateMode = true
+	m.pkgManager = NewEopkgManager(m, m.image.RootDir)
+
+	return nil
+}
+
 // Index will attempt to index the given directory for eopkgs.
 func (m *Manager) Index(dir string) error {
 	if m.IsCancelled() {
 		return ErrInterrupted
 	}
 
-	m.lock.Lock()
-	if m.pkg == nil {
-		m.lock.Unlock()
-		return ErrNoPackage
+	if err := m.checkPackage(); err != nil {
+		return err
 	}
-	m.lock.Unlock()
 
 	// Now get on with the real work!
-	defer m.Cleanup()
 	m.SigIntCleanup()
+	defer m.Cleanup()
 
 	// Now set our options according to the config
 	m.overlay.EnableTmpfs = m.Config.EnableTmpfs
@@ -497,4 +506,11 @@ func (m *Manager) SetTmpfs(enable bool, size string) {
 		m.Config.EnableTmpfs = enable
 		m.Config.TmpfsSize = strings.TrimSpace(size)
 	}
+}
+
+func (m *Manager) checkPackage() error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	return ErrNoPackage
 }

.golangci.yaml
@@ -1,6 +1,35 @@
 ---
+version: "2"
+
+issues:
+  max-issues-per-linter: 0
+  max-same-issues: 0
+
+severity:
+  default: major
+
+formatters:
+  enable:
+    - gci
+    - gofmt
+    - gofumpt
+
+  settings:
+    gci:
+      sections:
+        - standard
+        - default
+        - prefix(github.com/getsolus/solbuild)
+
+  exclusions:
+    generated: lax
+    paths:
+      - third_party$
+      - builtin$
+      - examples$
+
 linters:
-  enable-all: true
+  default: all
   disable:
     # Disabled to get codebase to pass the linter.
     # We can enable these one at a time.
@@ -17,99 +46,84 @@ linters:
     - noctx
     - perfsprint
     - revive
-    - stylecheck
     - wrapcheck
     # Disabled permanently
-    - exhaustruct     # structs may be uninitialized
-    - nlreturn        # covered by wsl cuddle rules
-    - paralleltest    # tests are acceptable in sequence
-    - goimports       # conflicts with GCI
     - depguard        # manage using go.mod for now
+    - exhaustruct     # structs may be uninitialized
+    - funcorder       # functions are allowed in any order
+    - nlreturn        # covered by wsl cuddle rules
+    - noinlineerr     # inline errors are allowed for readability
     - nonamedreturns  # named returns are acceptable in short functions
+    - paralleltest    # tests are acceptable in sequence
     # Deprecated
-    - execinquery
-    - exportloopref
-    - gomnd
-severity:
-  default-severity: major
+    - wsl
+  exclusions:
+    generated: lax
+    rules:
+      - linters:
+          - err113
+          - errcheck
+          - gochecknoglobals
+          - gosec
+          - wrapcheck
+        path: _test\.go
+      - linters: [staticcheck]
+        text: "ST1000:"
+    paths:
+      - third_party$
+      - builtin$
+      - examples$
+  settings:
+    exhaustive:
+      default-signifies-exhaustive: true
 
-issues:
-  fast: false
-  max-issues-per-linter: 0
-  max-same-issues: 0
-  exclude-use-default: false
-  exclude-case-sensitive: true
-  exclude-rules:
-    - path: _test\.go
-      linters:
-        - gochecknoglobals
-        - errcheck
-        - wrapcheck
-        - gosec
-        - goerr113
+    govet:
+      disable:
+        - fieldalignment
+      enable-all: true
 
-linters-settings:
-  varnamelen:
-    min-name-length: 1
+    misspell:
+      ignore-rules:
+        - evolveos
 
-  exhaustive:
-    default-signifies-exhaustive: true
+    revive:
+      enable-all-rules: false
+      rules:
+        - name: blank-imports
+        - name: context-as-argument
+        - name: context-keys-type
+        - name: dot-imports
+        - name: error-return
+        - name: error-strings
+        - name: error-naming
+        - name: exported
+        - name: if-return
+        - name: increment-decrement
+        - name: var-naming
+        - name: var-declaration
+        - name: package-comments
+        - name: range
+        - name: receiver-naming
+        - name: time-naming
+        - name: unexported-return
+        - name: indent-error-flow
+        - name: errorf
+        - name: empty-block
+        - name: superfluous-else
+        - name: unused-parameter
+        - name: unreachable-code
+        - name: redefines-builtin-id
 
-  gci:
-    sections:
-      - standard
-      - default
-      - prefix(github.com/getsolus/solbuild)
+    staticcheck:
+      checks:
+        - all
 
-  gomnd:
-    ignored-numbers: ['2', '4', '8', '16', '32', '64', '10']
+    tagalign:
+      order:
+        - zero
+        - short
+        - long
+        - desc
 
-  gosec:
-    excludes: []
-
-  govet:
-    enable-all: true
-    disable:
-      - fieldalignment  # misalignment is accepted
-
-  misspell:
-    ignore-words:
-      - evolveos
-
-  revive:
-    enable-all-rules: false
-    rules:  # see https://github.com/mgechev/revive#recommended-configuration
-      - name: blank-imports
-      - name: context-as-argument
-      - name: context-keys-type
-      - name: dot-imports
-      - name: error-return
-      - name: error-strings
-      - name: error-naming
-      - name: exported
-      - name: if-return
-      - name: increment-decrement
-      - name: var-naming
-      - name: var-declaration
-      - name: package-comments
-      - name: range
-      - name: receiver-naming
-      - name: time-naming
-      - name: unexported-return
-      - name: indent-error-flow
-      - name: errorf
-      - name: empty-block
-      - name: superfluous-else
-      - name: unused-parameter
-      - name: unreachable-code
-      - name: redefines-builtin-id
-
-  stylecheck:
-    checks: [all]
-
-  tagalign:
-    order:
-      - zero
-      - short
-      - long
-      - desc
+    varnamelen:
+      min-name-length: 1

builder/build.go
@@ -35,7 +35,7 @@ func (p *Package) CreateDirs(o *Overlay) error {
 
 	for _, p := range dirs {
 		if err := os.MkdirAll(p, 0o0755); err != nil {
-			return fmt.Errorf("Failed to create required directory %s. Reason: %w", p, err)
+			return fmt.Errorf("failed to create required directory %s. Reason: %w", p, err)
 		}
 	}
 
@@ -47,18 +47,18 @@ func (p *Package) CreateDirs(o *Overlay) error {
 
 			// Cache directories in build root.
 			if err := os.MkdirAll(inRootCacheDir, 0o0755); err != nil {
-				return fmt.Errorf("Failed to create cache directory %s in build root, reason: %w", inRootCacheDir, err)
+				return fmt.Errorf("failed to create cache directory %s in build root, reason: %w", inRootCacheDir, err)
 			}
 
 			// Cache directory in host.
 			// Ensure we have root owned cache directories.
 			if err := os.MkdirAll(hostCacheDir, 0o0755); err != nil {
-				return fmt.Errorf("Failed to create cache directory %s for %s, reason: %w", cache.CacheDir, cache.Name, err)
+				return fmt.Errorf("failed to create cache directory %s for %s, reason: %w", cache.CacheDir, cache.Name, err)
 			}
 
 			// Ensure the build user can write to the cache directories.
 			if err := os.Chown(hostCacheDir, BuildUserID, BuildUserGID); err != nil {
-				return fmt.Errorf("Failed to chown cache directory %s in build root, reason: %w", inRootCacheDir, err)
+				return fmt.Errorf("failed to chown cache directory %s in build root, reason: %w", inRootCacheDir, err)
 			}
 		}
 	}
@@ -76,7 +76,7 @@ func (p *Package) FetchSources(o *Overlay) error {
 		}
 
 		if err := source.Fetch(); err != nil {
-			return fmt.Errorf("Failed to fetch source %s, reason: %w\n", source.GetIdentifier(), err)
+			return fmt.Errorf("failed to fetch source %s, reason: %w", source.GetIdentifier(), err)
 		}
 	}
 
@@ -95,7 +95,7 @@ func (p *Package) BindSources(o *Overlay) error {
 		// Ensure sources tree exists
 		if !PathExists(sourceDir) {
 			if err := os.MkdirAll(sourceDir, 0o0755); err != nil {
-				return fmt.Errorf("Failed to create source directory %s, reason: %w\n", sourceDir, err)
+				return fmt.Errorf("failed to create source directory %s, reason: %w", sourceDir, err)
 			}
 		}
 
@@ -118,7 +118,7 @@ func (p *Package) BindSources(o *Overlay) error {
 
 		// Bind mount local source into chroot
 		if err := mountMan.BindMount(bindConfig.BindSource, bindConfig.BindTarget, "ro"); err != nil {
-			return fmt.Errorf("Failed to bind mount source %s, reason: %w\n", bindConfig.BindTarget, err)
+			return fmt.Errorf("failed to bind mount source %s, reason: %w", bindConfig.BindTarget, err)
 		}
 
 		// Account for these to help cleanups
@@ -128,10 +128,10 @@ func (p *Package) BindSources(o *Overlay) error {
 	return nil
 }
 
-// BindCache will make all cache defined in [caches] available to the build.
+// BindCaches will make all cache defined in [caches] available to the build.
 func (p *Package) BindCaches(o *Overlay) error {
 	if p.Type == PackageTypeXML {
-		return fmt.Errorf("Failed to bind caches, reason: not YPKG build")
+		return fmt.Errorf("failed to bind caches, reason: not YPKG build")
 	}
 
 	mountMan := disk.GetMountManager()
@@ -144,7 +144,7 @@ func (p *Package) BindCaches(o *Overlay) error {
 
 		// Bind mount local ccache into chroot
 		if err := mountMan.BindMount(cacheSource, cacheDir); err != nil {
-			return fmt.Errorf("Failed to bind mount %s %s, reason: %w\n", c.Name, cacheDir, err)
+			return fmt.Errorf("failed to bind mount %s %s, reason: %w", c.Name, cacheDir, err)
 		}
 
 		o.ExtraMounts = append(o.ExtraMounts, cacheDir)
@@ -238,12 +238,12 @@ func (p *Package) PrepYpkg(notif PidNotifier, usr *UserInfo, pman *EopkgManager,
 
 	if !PathExists(fpd) {
 		if err := os.MkdirAll(fpd, 0o0755); err != nil {
-			return fmt.Errorf("Failed to create packager directory %s, reason: %w\n", fpd, err)
+			return fmt.Errorf("failed to create packager directory %s, reason: %w", fpd, err)
 		}
 	}
 
 	if err := usr.WritePackager(fp); err != nil {
-		return fmt.Errorf("Failed to write packager file %s, reason: %w\n", fp, err)
+		return fmt.Errorf("failed to write packager file %s, reason: %w", fp, err)
 	}
 
 	wdir := p.GetWorkDirInternal()
@@ -258,7 +258,7 @@ func (p *Package) PrepYpkg(notif PidNotifier, usr *UserInfo, pman *EopkgManager,
 	slog.Debug("Installing build dependencies", "file", ymlFile)
 
 	if err := ChrootExec(notif, overlay.MountPoint, cmd); err != nil {
-		return fmt.Errorf("Failed to install build dependencies %s, reason: %w\n", ymlFile, err)
+		return fmt.Errorf("failed to install build dependencies %s, reason: %w", ymlFile, err)
 	}
 
 	notif.SetActivePID(0)
@@ -267,13 +267,13 @@ func (p *Package) PrepYpkg(notif PidNotifier, usr *UserInfo, pman *EopkgManager,
 	slog.Debug("Stopping D-BUS")
 
 	if err := pman.StopDBUS(); err != nil {
-		return fmt.Errorf("Failed to stop d-bus, reason: %w\n", err)
+		return fmt.Errorf("failed to stop d-bus, reason: %w", err)
 	}
 
 	// Chwn the directory before bringing up sources
 	cmd = fmt.Sprintf("chown -R %s:%s %s", BuildUser, BuildUser, BuildUserHome)
 	if err := ChrootExec(notif, overlay.MountPoint, cmd); err != nil {
-		return fmt.Errorf("Failed to set home directory permissions, reason: %w\n", err)
+		return fmt.Errorf("failed to set home directory permissions, reason: %w", err)
 	}
 
 	notif.SetActivePID(0)
@@ -304,7 +304,7 @@ func (p *Package) BuildYpkg(notif PidNotifier, usr *UserInfo, pman *EopkgManager
 
 	// Bring up sources
 	if err := p.BindSources(overlay); err != nil {
-		return fmt.Errorf("Failed to set home directory permissions, reason: %w\n", err)
+		return fmt.Errorf("failed to set home directory permissions, reason: %w", err)
 	}
 
 	// Reaffirm the layout
@@ -344,7 +344,7 @@ func (p *Package) BuildYpkg(notif PidNotifier, usr *UserInfo, pman *EopkgManager
 	slog.Info("Now starting build", "package", p.Name)
 
 	if err := ChrootExec(notif, overlay.MountPoint, cmd); err != nil {
-		return fmt.Errorf("Failed to start build of package, reason: %w\n", err)
+		return fmt.Errorf("failed to start build of package, reason: %w", err)
 	}
 
 	// Generate ABI Report
@@ -374,7 +374,7 @@ func (p *Package) BuildXML(notif PidNotifier, pman *EopkgManager, overlay *Overl
 
 	// Bring up sources
 	if err := p.BindSources(overlay); err != nil {
-		return fmt.Errorf("Cannot continue without sources.\n")
+		return fmt.Errorf("cannot continue without sources")
 	}
 
 	// Now recopy the assets prior to build
@@ -391,7 +391,7 @@ func (p *Package) BuildXML(notif PidNotifier, pman *EopkgManager, overlay *Overl
 	slog.Info("Now starting build", "package", p.Name)
 
 	if err := ChrootExec(notif, overlay.MountPoint, cmd); err != nil {
-		return fmt.Errorf("Failed to start build of package.\n")
+		return fmt.Errorf("failed to start build of package")
 	}
 
 	notif.SetActivePID(0)
@@ -400,7 +400,7 @@ func (p *Package) BuildXML(notif PidNotifier, pman *EopkgManager, overlay *Overl
 	slog.Debug("Stopping D-BUS")
 
 	if err := pman.StopDBUS(); err != nil {
-		return fmt.Errorf("Failed to stop d-bus, reason: %w\n", err)
+		return fmt.Errorf("failed to stop d-bus, reason: %w", err)
 	}
 
 	notif.SetActivePID(0)
@@ -440,7 +440,7 @@ func (p *Package) CollectAssets(overlay *Overlay, usr *UserInfo, manifestTarget 
 		tram := NewTransitManifest(manifestTarget)
 		for _, p := range collections {
 			if err := tram.AddFile(p); err != nil {
-				return fmt.Errorf("Failed to collect eopkg asset for transit manifest %s, reason: %w\n", p, err)
+				return fmt.Errorf("failed to collect eopkg asset for transit manifest %s, reason: %w", p, err)
 			}
 		}
 
@@ -472,13 +472,13 @@ func (p *Package) CollectAssets(overlay *Overlay, usr *UserInfo, manifestTarget 
 	for _, p := range collections {
 		tgt, err := filepath.Abs(filepath.Join(".", filepath.Base(p)))
 		if err != nil {
-			return fmt.Errorf("Unable to find working directory, reason: %w\n", err)
+			return fmt.Errorf("unable to find working directory, reason: %w", err)
 		}
 
 		slog.Debug("Collecting build artifact", "path", filepath.Base(p))
 
 		if err = disk.CopyFile(p, tgt); err != nil {
-			return fmt.Errorf("Unable to collect build file, reason: %w\n", err)
+			return fmt.Errorf("unable to collect build file, reason: %w", err)
 		}
 
 		slog.Debug("Setting file ownership for current user", "uid", usr.UID, "gid", usr.GID, "path", filepath.Base(p))
@@ -519,7 +519,7 @@ func (p *Package) Build(notif PidNotifier, history *PackageHistory, profile *Pro
 
 	// Ensure source assets are in place
 	if err := p.CopyAssets(history, overlay); err != nil {
-		return fmt.Errorf("Failed to copy required source assets, reason: %w\n", err)
+		return fmt.Errorf("failed to copy required source assets, reason: %w", err)
 	}
 
 	slog.Debug("Validating sources")
@@ -537,24 +537,24 @@ func (p *Package) Build(notif PidNotifier, history *PackageHistory, profile *Pro
 	slog.Debug("Starting D-BUS")
 
 	if err := pman.StartDBUS(); err != nil {
-		return fmt.Errorf("Failed to start d-bus, reason: %w\n", err)
+		return fmt.Errorf("failed to start d-bus, reason: %w", err)
 	}
 
 	// Get the repos in place before asserting anything
 	if err := p.ConfigureRepos(notif, overlay, pman, profile); err != nil {
-		return fmt.Errorf("Configuring repositories failed, reason: %w\n", err)
+		return fmt.Errorf("configuring repositories failed, reason: %w", err)
 	}
 
 	slog.Debug("Upgrading system base")
 
 	if err := pman.Upgrade(); err != nil {
-		return fmt.Errorf("Failed to upgrade rootfs, reason: %w\n", err)
+		return fmt.Errorf("failed to upgrade rootfs, reason: %w", err)
 	}
 
 	slog.Debug("Asserting system.devel component installation")
 
 	if err := pman.InstallComponent("system.devel"); err != nil {
-		return fmt.Errorf("Failed to assert system.devel, reason: %w\n", err)
+		return fmt.Errorf("failed to assert system.devel, reason: %w", err)
 	}
 
 	// Ensure all directories are in place

builder/copy.go
@@ -59,7 +59,7 @@ func CopyAll(source, destdir string) error {
 			slog.Debug("Creating target directory", "dir", destdir)
 
 			if err = os.MkdirAll(destdir, 0o0755); err != nil {
-				return fmt.Errorf("Failed to create target directory: %s, reason: %w\n", destdir, err)
+				return fmt.Errorf("failed to create target directory: %s, reason: %w", destdir, err)
 			}
 		}
 
@@ -67,7 +67,7 @@ func CopyAll(source, destdir string) error {
 		slog.Debug("Copying source", "source", source, "target", tgt)
 
 		if err = disk.CopyFile(source, tgt); err != nil {
-			return fmt.Errorf("Failed to copy source asset to target: source='%s' target='%s', reason: %w\n", source, tgt, err)
+			return fmt.Errorf("failed to copy source asset to target: source=%q, target=%q, reason: %w", source, tgt, err)
 		}
 	}
 

builder/eopkg.go
@@ -95,14 +95,14 @@ func (e *EopkgManager) CopyAssets() error {
 			slog.Debug("Creating required directory", "path", dirName)
 
 			if err := os.MkdirAll(dirName, 0o0755); err != nil {
-				return fmt.Errorf("Failed to create required asset directory %s, reason %w\n", dirName, err)
+				return fmt.Errorf("failed to create required asset directory %s, reason %w", dirName, err)
 			}
 		}
 
 		slog.Debug("Copying host asset", "key", key)
 
 		if err := disk.CopyFile(key, value); err != nil {
-			return fmt.Errorf("Failed to copy host asset %s, reason: %w\n", key, err)
+			return fmt.Errorf("failed to copy host asset %s, reason: %w", key, err)
 		}
 	}
 
@@ -127,7 +127,7 @@ func (e *EopkgManager) Init() error {
 		slog.Debug("Creating system-wide package cache", "path", e.cacheSource)
 
 		if err := os.MkdirAll(e.cacheSource, 0o0755); err != nil {
-			return fmt.Errorf("Failed to create package cache %s, reason: %w\n", e.cacheSource, err)
+			return fmt.Errorf("failed to create package cache %s, reason: %w", e.cacheSource, err)
 		}
 	}
 
@@ -249,12 +249,12 @@ func EnsureEopkgLayout(root string) error {
 	runPath := filepath.Join(root, "run")
 	if PathExists(runPath) {
 		if err := os.RemoveAll(runPath); err != nil {
-			return fmt.Errorf("Failed to clean stale /run, reason: %w\n", err)
+			return fmt.Errorf("failed to clean stale /run, reason: %w", err)
 		}
 	}
 
 	if err := os.MkdirAll(runPath, 0o0755); err != nil {
-		return fmt.Errorf("Failed to clean stale /run, reason: %w\n", err)
+		return fmt.Errorf("failed to clean stale /run, reason: %w", err)
 	}
 
 	// Construct the required directories in the tree
@@ -334,6 +334,7 @@ func (e *EopkgManager) GetRepos() ([]*EopkgRepo, error) {
 // AddRepo will attempt to add a repo to the filesystem.
 func (e *EopkgManager) AddRepo(id, source string) error {
 	e.notif.SetActivePID(0)
+
 	return ChrootExec(e.notif, e.root,
 		eopkgCommand(fmt.Sprintf("%s add-repo '%s' '%s'", installCommand, id, source)))
 }
@@ -341,6 +342,7 @@ func (e *EopkgManager) AddRepo(id, source string) error {
 // RemoveRepo will attempt to remove a named repo from the filesystem.
 func (e *EopkgManager) RemoveRepo(id string) error {
 	e.notif.SetActivePID(0)
+
 	return ChrootExec(e.notif, e.root,
 		eopkgCommand(fmt.Sprintf("%s remove-repo '%s'", installCommand, id)))
 }

builder/lockfile.go
@@ -27,11 +27,11 @@ import (
 
 var (
 	// ErrDeadLockFile is returned when an dead lockfile was encountered.
-	ErrDeadLockFile = errors.New("Dead lockfile")
+	ErrDeadLockFile = errors.New("dead lockfile")
 
 	// ErrOwnedLockFile is returned when the lockfile is already owned by
 	// another active process.
-	ErrOwnedLockFile = errors.New("File is locked")
+	ErrOwnedLockFile = errors.New("file is locked")
 )
 
 // A LockFile encapsulates locking functionality.

builder/main.go
@@ -27,7 +27,7 @@ import (
 // Spelled this way so people don't get confused :P.
 var DisableColors bool
 
-// Controls whether or not we generate an ABI report.
+// DisableABIReport controls whether or not we generate an ABI report.
 var DisableABIReport bool
 
 const (

builder/namespaces.go
@@ -27,7 +27,7 @@ func ConfigureNamespace() error {
 	slog.Debug("Configuring container namespace")
 
 	if err := syscall.Unshare(syscall.CLONE_NEWNS | syscall.CLONE_NEWIPC); err != nil {
-		return fmt.Errorf("Failed to configure namespace, reason: %w\n", err)
+		return fmt.Errorf("failed to configure namespace, reason: %w", err)
 	}
 
 	return nil
@@ -38,7 +38,7 @@ func DropNetworking() error {
 	slog.Debug("Dropping container networking")
 
 	if err := syscall.Unshare(syscall.CLONE_NEWNET | syscall.CLONE_NEWUTS); err != nil {
-		return fmt.Errorf("Failed to drop networking capabilities, reason: %w\n", err)
+		return fmt.Errorf("failed to drop networking capabilities, reason: %w", err)
 	}
 
 	return nil

commit fde6b88836d89c81ec3216eeaa47de0ae18afff4

Fix "you must first set a package to build it" error (#131)
Fix an issue introduced in #130 where all builds fail due to the package

Makefile
@@ -1,4 +1,4 @@
-VERSION := 1.8.0
+VERSION := 1.8.1
 BINNAME := solbuild
 
 .PHONY: build

builder/manager.go
@@ -405,14 +405,9 @@ func (m *Manager) Chroot() error {
 		return ErrInterrupted
 	}
 
-	m.lock.Lock()
-
-	if m.pkg == nil {
-		m.lock.Unlock()
-		return ErrNoPackage
+	if err := m.checkPackage(); err != nil {
+		return err
 	}
-
-	m.lock.Unlock()
 
 	// Now get on with the real work!
 	defer m.Cleanup()
@@ -512,5 +507,9 @@ func (m *Manager) checkPackage() error {
 	m.lock.Lock()
 	defer m.lock.Unlock()
 
-	return ErrNoPackage
+	if m.pkg == nil {
+		return ErrNoPackage
+	}
+
+	return nil
 }